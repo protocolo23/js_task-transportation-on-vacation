6e18853671bb3e8bf1b22119acb7c3cc2a6145c6
protocolo23 <narashikamaro27@gmail.com>
2025-07-15 10:59:44 -0300

    solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,19 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const DAILY_RATE = 40;
+  const DISCOUNT_FOR_3_DAYS = 20;
+  const DISCOUNT_FOR_7_DAYS = 50;
+
+  let total = days * DAILY_RATE;
+
+  if (days >= 7) {
+    total -= DISCOUNT_FOR_7_DAYS;
+  } else if (days >= 3) {
+    total -= DISCOUNT_FOR_3_DAYS;
+  }
+
+  return total;
 }
 
 module.exports = calculateRentalCost;
